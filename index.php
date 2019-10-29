62f1c6a1d67c11e759cf3e87ddaf8ee9d73a9a7f
Ravi goswami <zneroravi@gmail.com>
2019-10-30 05:28:06 +0530

    seo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-  <meta name="description" content="digital certificate plateform, give my certificate">
+  <meta name="description" content="certificate generator">
   <meta name="keywords" content="digital certificate, certificate, digital india">
   <meta name="viewport" content= "width=device-width, initial-scale=1.0"> 
 
