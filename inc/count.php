df40ca89f3901bd7744646c6452f33ec8c2e1a7c
Ravi goswami <zneroravi@gmail.com>
2019-11-16 15:25:07 +0530

    aa

--- inc/count.php
+++ inc/count.php
@@ -13,7 +13,7 @@
                             </div>
                             <!--end col-sm-4-->
                             <?php 
-                                $cert_count = 41216;
+                                $cert_count = 45216;
                                 $tree_count = ($cert_count/2400);
                                 $tree_count = (int)$tree_count;
                             ?>
