e0d47320cf6b018e43a1bdb4988a2e237513d64b
Ravi goswami <zneroravi@gmail.com>
2020-05-04 00:41:29 +0530

    update

--- index.php
+++ index.php
@@ -17,8 +17,152 @@
     <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="shortcut icon" href="assets/img/favicon.ico" />
+    <link
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous"
+    />
 	<title>Give My Certificate</title>
-
+<style>
+      .soft {
+        background-color: black !important;
+        height: 100vh;
+        margin: 0 auto;
+      }
+      .element {
+        height: 200px;
+        background: #5ce7e7;
+        position: relative;
+      }
+      .element2 {
+        height: 200px;
+        background: #36d6d6;
+        position: relative;
+      }
+      .element3 {
+        height: 200px;
+        background: #5ce7e7;
+        position: relative;
+      }
+      .element4 {
+        height: 200px;
+        background: #36d6d6;
+        position: relative;
+      }
+      .element:before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        border-top: 42px solid black;
+        border-right: 42px solid #5ce7e7;
+        width: 0;
+      }
+      .element2:before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        border-top: 42px solid #36d6d6;
+        border-left: 42px solid black;
+        width: 0;
+      }
+      .element3:before {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        border-top: 42px solid #5ce7e7;
+        border-right: 42px solid black;
+        width: 0;
+      }
+      .element4:before {
+        content: "";
+        position: absolute;
+        top: 0;
+        right: 0;
+        border-bottom: 42px solid #36d6d6;
+        border-right: 42px solid black;
+        width: 0;
+      }
+      .color {
+        color: #36d6d6;
+        font-size: 1.4em;
+        font-weight: bolder;
+      }
+      .text-white {
+        text-align: right;
+        font-weight: 300;
+        font-size: 1.2em;
+        font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+      }
+      .text-white1 {
+        text-align: left;
+        font-weight: 300;
+        font-size: 1.2em;
+        font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+        color: white;
+      }
+      body {
+        background-color: black;
+      }
+      .padr {
+        padding-right: 1.7em;
+      }
+      .padl {
+        padding-left: 1.7em;
+      }
+      .display-3 {
+        font-weight: bolder;
+        font-size: 5.2em;
+        color: white;
+      }
+      .order {
+        order: unset;
+      }
+      .bg-dark {
+        background-color: black !important;
+      }
+      .orderx {
+        order: unset;
+      }
+      .pt-5{
+          color:white !important;
+      }
+      @media (max-width: 425px) {
+        .order {
+          order: 2;
+        }
+        .bg-dark {
+          padding: 3em 1em;
+        }
+        .orderx {
+          order: 1;
+        }
+        .pd {
+          padding: 0em 1em;
+        }
+        .text-right {
+          text-align: center !important;
+        }
+        .text-white {
+          text-align: center !important;
+        }
+        .text-left {
+          text-align: center !important;
+        }
+        .text-white1 {
+          text-align: center !important;
+        }
+        .padr {
+          padding: 0;
+        }
+        .padl {
+          padding: 0;
+        }
+      }
+    </style>
 </head>
 <body data-spy="scroll" data-target=".navbar" class="has-loading-screen">
     <div class="ts-page-wrapper" id="page-top">
