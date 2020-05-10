658ff3e3249c3da9e6aff5de0ffb80e00a8b6a0a
Rizul <rizulsharma.info@gmail.com>
2020-05-10 21:46:18 +0530

    Done setting corona warrior panel

--- assets/js/corona-warrior.js
+++ assets/js/corona-warrior.js
@@ -1,16 +1,28 @@
 (function($){
 	function CoronaWarriorPanel() {
+		const emailRegEx = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		const phoneRegEx = /^[9,8,6][0-9]{9}$/;
+		const fullnameRegEx = /^[a-zA-z\s]+$/;
+
 		const getNode = function(selector) {
 			return $(selector);
 		}
 
 		const fieldMapping = {
-			fullname: { isRequired: true, testExp: '/^[a-zA-z\s]+$/' },
-			email: { isRequired: true, testExp: '' },
-			phone: { isRequired: false, testExp: '' },
+			fullname: { isRequired: true, testFunc: val => fullnameRegEx.test(val) },
+			email: { isRequired: true, testFunc: val => emailRegEx.test(val) },
+			phone: { isRequired: false, testFunc: val => phoneRegEx.test(val) },
 			profession: { isRequired: true, testFunc: (i) => parseInt(i) !== -1},
 			lockdownExpRating: { isRequired: true, testFunc: (i) => parseInt(i) > 0 },
 			lockdownExpDesc: { isRequired: false, testFunc: (s) => s.trim().length > 50 }
+		}
+
+		const resetErr = function(e) {
+			const field = e.target;
+			const classList = field.classList;
+			if (classList.contains('error')) {
+				field.classList.remove('error');
+			}
 		}
 		
 		return function() {
@@ -21,6 +33,7 @@
 				this.userDetailForm = getNode('#userDetailForm');
 				this.userDetailForm.on('submit', this.handleFormSubmit.bind(this));
 				this.form = this.userDetailForm;
+				this.attachErrResetEvent();
 			}
 
 			this.handleFormSubmit = function(e) {
@@ -31,8 +44,26 @@
 					const field = formFields[i];
 					const fieldName = field.name;
 					const value = field.value;
+					const fieldMap = fieldMapping[fieldName];
+					if(fieldMap.isRequired || value.trim().length > 0) {
+						if(!fieldMap.testFunc(value)) {
+							const node = getNode('#'+fieldName);
+							node.addClass('error');
+							// const parentNode = node.parent();
+							// if(!parentNode.hasCalss('error-msg')) {
+							// 	const errMsg = '<span class="err">field is required</span>'
+							// 	parentNode.addClass('error-msg').append();
+							// }
+						}
+					}
 				}
 				return false;
+			}
+
+			this.attachErrResetEvent = function() {
+				Object.keys(fieldMapping).forEach((field, id) => {
+					$('#'+field).change(resetErr);
+				})
 			}
 
 			// invoking constructor
