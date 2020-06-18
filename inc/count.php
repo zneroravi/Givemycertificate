a1644f78958eed4a14070faf6900a9d1224d62db
Ravi goswami <zneroravi@gmail.com>
2020-06-18 12:03:50 +0530

    upload count

--- inc/count.php
+++ inc/count.php
@@ -13,7 +13,7 @@
                             </div>
                             <!--end col-sm-4-->
                             <?php 
-                                $cert_count = 882537;
+                                $cert_count = 884872;
                                 $tree_count = ($cert_count/2400);
                                 $tree_count = (int)$tree_count;
                             ?>
