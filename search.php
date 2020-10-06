6983835f37860f79373d8d0d38615ff8acbec0f6
Ravi goswami <zneroravi@gmail.com>
2020-10-07 00:59:10 +0530

    a

--- search.php
+++ search.php
@@ -12,7 +12,6 @@
   crossorigin="anonymous"
 />
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-<link rel="shortcut icon" href="../assets/img/favicon.ico" />
 
   <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
