a37593f3a2ea98ccb506c66fa878b6ba0d276e93
Ravi goswami <zneroravi@gmail.com>
2020-10-11 14:02:43 +0530

    upload

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="give my certificate" />
+  <meta name="keywords" content="givemycertificate, give my certificate, certificate, gmc" />
   <link rel="shortcut icon" href="style/images/favicon.ico">
   <title>Give My Certificate</title>
   <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
--- sponsorship.php
+++ sponsorship.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="give my certificate sponsorship" />
+  <meta name="keywords" content="sponsorship, givemycertificate" />
   <link rel="shortcut icon" href="style/images/favicon.ico">
   <title>Sponsorship | Give My Certificate</title>
   <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
