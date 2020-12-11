fc840121824fbfb0497ceca44e9d2ab469ccdd88
Ravi goswami <zneroravi@gmail.com>
2020-12-11 16:18:11 +0530

    update

--- inc/counter.php
+++ inc/counter.php
@@ -10,7 +10,7 @@
               </div>
               <!--/column -->
               <?php 
-                $cert_count = 1294185;
+                $cert_count = 1311525;
                 $tree_count = ($cert_count/2400);
                 $tree_count = (int)$tree_count;
               ?>
