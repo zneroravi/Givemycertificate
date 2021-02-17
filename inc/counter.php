bfe3ef535160f183a98814df0354898fec5e2b08
Ravi goswami <zneroravi@gmail.com>
2021-02-17 16:22:10 +0530

    update ount

--- inc/counter.php
+++ inc/counter.php
@@ -10,7 +10,7 @@
               </div>
               <!--/column -->
               <?php 
-                $cert_count = 1614394;
+                $cert_count = 1622856;
                 $tree_count = ($cert_count/2400);
                 $tree_count = (int)$tree_count;
               ?>
