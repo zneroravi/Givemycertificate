4eb954c6190d50372a5623ab2c4610aef15de302
Ravi goswami <zneroravi@gmail.com>
2021-05-17 15:31:30 +0530

    update count

--- inc/counter.php
+++ inc/counter.php
@@ -5,12 +5,12 @@
           <div class="col-lg-10 offset-lg-1">
             <div class="row counter">
               <div class="col-md-4 text-center">
-                <h3 class="text">900+</h3>
+                <h3 class="text">1000+</h3>
                 <p><strong>Clients</strong></p>
               </div>
               <!--/column -->
               <?php 
-                $cert_count = 2062014;
+                $cert_count = 2079837;
                 $tree_count = ($cert_count/2400);
                 $tree_count = (int)$tree_count;
               ?>
