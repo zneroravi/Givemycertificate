36769a83ac43259429aa389759c9d15bd9a3a636
Ravi goswami <zneroravi@gmail.com>
2021-07-01 12:57:40 +0530

    update count

--- inc/counter.php
+++ inc/counter.php
@@ -10,7 +10,7 @@
               </div>
               <!--/column -->
               <?php 
-                $cert_count = 2238482;
+                $cert_count = 2253285;
                 $tree_count = ($cert_count/2400);
                 $tree_count = (int)$tree_count;
               ?>
