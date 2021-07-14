41dbeb7b6ebb006d27f543bc904aa5cd3d63f673
Ravi goswami <zneroravi@gmail.com>
2021-07-14 23:48:12 +0530

    add pricing table

---
+++ pricing.php
@@ -0,0 +1,396 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="give my certificate contact" />
+  <meta name="keywords" content="contact, givemycertificate, give my certificate, certificate" />
+  <link rel="shortcut icon" href="style/images/favicon.ico">
+  <title>Pricing | Give My Certificate</title>
+  <link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="style/css/plugins.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/settings.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/layers.css">
+  <link rel="stylesheet" type="text/css" href="style/revolution/css/navigation.css">
+  <link rel="stylesheet" type="text/css" href="style/type/type.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="style/css/color/purple.css">
+</head>
+<style>
+  .banner--unstick .navbar-brand a{
+    color: white !important;
+   
+  }
+  .banner--stick .navbar-brand a{
+    color: #5f5757 !important;
+    
+  }
+  @media (max-width: 576px){
+    .hideen{
+        display: none;
+    }
+}
+</style>
+<body>
+  <div class="content-wrapper">
+    <?php
+    include 'inc/nav2.html';
+    ?>
+    <!-- /.offcanvas-info -->
+    <div class="wrapper image-wrapper bg-image page-title-wrapper inverse-text" data-image-src="style/images/art/bg1.jpg">
+      <div class="container inner text-center">
+        <div class="space90"></div>
+        <h1 class="page-title">Pricing Tables</h1>
+        <p class="lead">Contact us for custom model</p>
+      </div>
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
+    <div class="wrapper gray-wrapper">
+      <div class="container inner">
+        <h2 class="section-title mb-40 text-center">Pricing Table</h2>
+        <div class="pricing-wrapper">
+          <div class="pricing-switcher-wrapper switcher">
+            <div class="pricing-switchers">
+              <div class="pricing-switcher pricing-switcher-active">$ Dollar</div>
+              <div class="pricing-switcher">₹ Rupee</div>
+              <div class="switcher-button bg-default"></div>
+            </div>
+          </div>
+          <div class="space20"></div>
+          <div class="row">
+            <div class="col-md-8 col-lg-8 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">0</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">₹</span><span class="price-value">0</span> <span class="price-duration">month</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Free Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>150</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>1</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Dedicated</strong> Profile Page </td>
+                    </tr>
+                    <tr>
+                      <td><strong>1</strong> Event Publish </td>
+                    </tr>
+                    <tr>
+                      <td><strong>15+ Years</strong> Verification </td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> - <strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <div class="col-md-8 col-lg-8 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">9</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">₹</span><span class="price-value">599</span> <span class="price-duration">month</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Basic Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>300</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>1</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Dedicated</strong> Profile Page </td>
+                    </tr>
+                    <tr>
+                      <td><strong>3</strong> Event Publish </td>
+                    </tr>
+                    <tr>
+                      <td><strong>15+ Years</strong> Verification </td>
+                    </tr>
+                    <tr>
+                      <td> Custom Email<strong> Content</strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong>  </strong></td>
+                    </tr>
+                    <tr>
+                      <td> Verification<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <div class="col-md-8 col-lg-8 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">40</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">₹</span><span class="price-value">3000</span> <span class="price-duration">month</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Startup Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>1000</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>2</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Dedicated</strong> Profile Page </td>
+                    </tr>
+                    <tr>
+                      <td><strong>10</strong> Event Publish </td>
+                    </tr>
+                    <tr>
+                      <td><strong>15+ Years</strong> Verification </td>
+                    </tr>
+                    <tr>
+                      <td> Custom Email<strong> Content</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Cryto<strong>  Certificate</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Verification<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Event<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Certificate<strong> API</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+            <div class="col-md-6 col-lg-6 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">110</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">₹</span><span class="price-value">8000</span><span class="price-duration">month</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Plus Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>3000</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>4</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Dedicated</strong> Profile Page </td>
+                    </tr>
+                    <tr>
+                      <td><strong>30</strong> Event Publish </td>
+                    </tr>
+                    <tr>
+                      <td><strong>15+ Years</strong> Verification </td>
+                    </tr>
+                    <tr>
+                      <td> Custom Email<strong> Content</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Crypto<strong>  Certificate</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Verification<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Event<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Certificate<strong> API</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+            <div class="space30 d-none d-md-block d-lg-block d-xl-none"></div>
+            <div class="col-md-6 col-lg-6 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class="price price-show"><span class="price-currency">$</span><span class="price-value">170</span> <span class="price-duration">month</span></div>
+                    <div class="price price-hide price-hidden"><span class="price-currency">₹</span><span class="price-value">12500</span> <span class="price-duration">month</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Advanced Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>5000</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>5</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Dedicated</strong> Profile Page </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Unlimited</strong> Event Publish </td>
+                    </tr>
+                    <tr>
+                      <td><strong>15+ Years</strong> Verification </td>
+                    </tr>
+                    <tr>
+                      <td> Custom Email<strong> Content</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Crypto<strong>  Certificate</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Verification<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Event<strong> API</strong></td>
+                    </tr>
+                    <tr>
+                      <td> Certificate<strong> API</strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+
+              <!--/.pricing -->
+            </div>
+            
+            <!--/column -->
+            <div class="col-md-6 col-lg-6 col-xl-3">
+              <div class="pricing panel box bg-white shadow">
+                <div class="panel-heading">
+                  <div class="prices color-dark">
+                    <div class=""><span class="price-currency"></span><span class="price-value">Contact us</span></div>
+                  </div>
+                  <h4 class="panel-title mb-0">Premium Plan</h4>
+                </div>
+                <!--/.panel-heading -->
+                <div class="panel-body">
+                  <table class="table">
+                    <tr>
+                      <td><strong>Custom</strong> Certificates </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Custom</strong> Organisations </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Advanced</strong> plan Features </td>
+                    </tr>
+                    <tr>
+                      <td><strong>Custom Verification URL</strong></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Custom logo</strong> on QR Code </td>
+                    </tr>
+                    <tr>
+                      <td>-<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong></strong></td>
+                    </tr>
+                    <tr>
+                      <td> -<strong> </strong></td>
+                    </tr>
+                  </table>
+                </div>
+                <!--/.panel-body -->
+                <div class="panel-footer"> <a href="#" class="btn" role="button">Choose Plan</a></div>
+              </div>
+              <!--/.pricing -->
+            </div>
+            <!--/column -->
+            <!--/column -->
+          </div>
+          <!--/.row -->
+        </div>
+        <!--/.pricing-wrapper -->
+        <div class="space20"></div>
+        <p>* ₹4/certificate overages apply.</p>
+      </div>
+
+      <!-- /.container -->
+    </div>
+    <!-- /.wrapper -->
+    <?php
+    include 'inc/footer.html';
+    ?>
+  </div>
+  <!-- /.content-wrapper -->
+  <script src="style/js/jquery.min.js"></script>
+  <script src="style/js/popper.min.js"></script>
+  <script src="style/js/bootstrap.min.js"></script>
+  <script src="style/revolution/js/jquery.themepunch.tools.min.js"></script>
+  <script src="style/revolution/js/jquery.themepunch.revolution.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+  <script src="style/revolution/js/extensions/revolution.extension.video.min.js"></script>
+  <script src="style/js/plugins.js"></script>
+  <script src="style/js/scripts.js"></script>
+</body>
+</html>
