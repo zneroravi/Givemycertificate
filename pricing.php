70fd9fe4eb3211d10868b657e91a79f9ab52646c
Ravi goswami <zneroravi@gmail.com>
2021-07-18 00:02:26 +0530

    add analytics

--- pricing.php
+++ pricing.php
@@ -31,6 +31,15 @@
     }
 }
 </style>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-173970539-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-173970539-1');
+</script>
 <body>
   <div class="content-wrapper">
     <?php
